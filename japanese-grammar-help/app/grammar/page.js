9263b22f9c33d2754f09010b9cb97bf926ec29b1
Petteri Leino <petewaterz@gmail.com>
2024-02-08 11:49:20 +0200

    cards are now toggleable. Removed annoying ripple effect on click

--- japanese-grammar-help/app/grammar/page.js
+++ japanese-grammar-help/app/grammar/page.js
@@ -1,36 +1,40 @@
+"use client"
+
 import React from "react";
 import {Card, CardHeader, CardBody, Divider} from "@nextui-org/react";
-import { promises as fs } from 'fs';
 import {tiedot} from "./testi.js"
 
-export default async function Grammar() {
-    const res = await fs.readFile(process.cwd() + "/app/grammar/grammar.json", "utf-8")
-    const grammarData = JSON.parse(res);
-    // console.log(grammarData)
-    // console.log(grammarData[0].content)
-    console.log(tiedot[1].content)
+// className="md:hidden" tällä saa piiloon elementin
 
-    const kokeilu = "<br>apina ja <i>gorilla</i>"
-    const seuraava = {__html:kokeilu};
+export default async function Grammar() {
+    const toggleCard = function(e){
+        const toggle = e.target.querySelector(".toggleThis")
+        // console.log(toggle)
+        toggle.classList.toggle("hidden")
+
+    }
+
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="flex flex-col z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <div className="flex flex-col z-10 w-5xl w-full items-center justify-between font-mono text-sm">
         {tiedot.map((card) =>      
         <Card
-        key="{card}" 
-        className="bg-zinc-500 max-w-[400px] p-10 my-10 rounded-xl"
+        key={card.title}
+        className="bg-zinc-500 w-3/4 p-10 my-10 rounded-xl"
         shadow="lg"
-        fullWidth="true">
+        fullWidth="true"
+        isPressable onPress={(e) => toggleCard(e)}
+        disableRipple="true">
       <CardHeader className="flex gap-3">
-        <div className="flex flex-col">
-          <p className="text-black-500 font-bold text-lg">{card.title}</p>
+        <div className="flex w-full gap-3 justify-between">
+          <p className="flex text-black-500 font-bold text-lg">{card.title}</p>
+          <p className="flex">Klikkaa</p>
         </div>
       </CardHeader>
-      <Divider/>
-      <CardBody>
+      <Divider />
+      <CardBody className="toggleThis hidden">
         <p dangerouslySetInnerHTML={{__html:card.content}}></p>
       </CardBody>
-      <Divider/>
     </Card>)}
 
         </div>
--- japanese-grammar-help/app/grammar/testi.js
+++ japanese-grammar-help/app/grammar/testi.js
@@ -26,7 +26,59 @@ const tiedot = [
     <br>
     <br>あのゲームを<strong>買ってもいい</strong>よ。
     <br><i>Ano geemu o katte mo ii yo.</i>
-    <br>Voit ostaa sen pelin.`}
+    <br>Voit ostaa sen pelin.`},
+    {"title": "~てはいけません / ~te wa ikemasen / kieltäminen",
+    "content": `<br><strong><i>! Jos te-muoto ei ole enää tuoreessa mielessä, katso Kappale 4 - Verbien mennyt aika (+ TE-MUOTO)</i></strong>
+        <br>Kieltäminen tapahtuu lisäämällä verbin -te muodon perään <strong>はいけません</strong>
+        <br>レストランでタバコを<strong>吸ってはいけません</strong>。
+        <br><i>Resutoran de tabako o sutte wa ikemasen.</i>
+        <br>Ravintolassa ei saa polttaa tupakkaa.
+
+        <br><br>いけません on kohteliaampi muoto verbistä いけない jota myös voi käyttää. Yksinkertaisin ja kasuaalein käytettävä sana on <strong>だめ</strong>.
+        <br>危ない場所に<strong>行ってはだめ</strong>よ。
+        <br><i>Abunai basho ni itte wa dame yo.</i>
+        <br>Vaarallisiin paikkoihin ei pidä mennä!`},
+    {"title": "Adjektiivien luetteleminen",
+        "content": `Tapa jolla adjektiivit yhdistetään riippuu siitä ovatko ne i- vai na-adjektiiveja. Jos kyseessä on i-adjektiivi, putoaa viimeinen い pois
+        ja sen tilalle tulee ~<strong>くて</strong>. Jos kyseessä on na-adjektiivi, korvataan な hiraganalla <strong>で</strong>. Viimeistä adjektiivia ei enää 
+        taivuteta tällä tavoin. Lauseen aikamuoto ilmenee vasta lauseen lopussa!
+        <br>怖<strong>くて</strong>大きい熊です。
+        <br><i>Kowakute ookii kuma desu.</i>
+        <br>Pelottava <strong>ja</strong> iso karhu.
+        <br>
+        <br>静か<strong>で</strong>綺麗な公園です。
+        <br><i>Shizuka de kirei na kouen desu.</i>
+        <br>Rauhallinen <strong>ja</strong> kaunis puisto.
+        <br>
+        <br>この車は便利<strong>で</strong>速いです。
+        <br><i>Kono kuruma wa benri de hayai desu.</i>
+        <br>Tämä auto on käytännöllinen <strong>ja</strong> nopea.
+        <br>
+        <br>Huom, lauseessa kaikkien adjektiivien on oltava merkitykseltään joko kielteisiä tai myönteisiä. Muussa tapauksessa tarvitsee käyttää sanaa mutta,
+        esim. <strong>が</strong>
+        <br>映画は面白い<strong>ですが</strong>、とても長かったです。
+        <br><i>Eiga wa omoshiroi desu ga, totemo nagakatta desu.</i>
+        <br>Elokuva <strong><i>oli</i></strong> viihdyttävä, <strong>mutta</strong> todella pitkä.`},
+        {"title": "Lauseiden yhdistäminen",
+        "content": `Lauseiden yhdistäminen riippuu täysin siitä, minkälainen ensimmäinen lause on. Jos kyseessä on verbi- tai i-adjektiivilause, ne yhdistetään taivuttamalla
+         ensimmäisen lauseen verbi tai adjektiivi te-muotoon. (ks. edellinen kielioppiasia)
+        <br>毎朝、新聞を<strong>読んで</strong>、ジュースを飲みます。
+        <br><i>Maiasa shinbun o yonde, juusu o nomimasu.</i>
+        <br>Joka aamu luen sanomalehteä <strong>ja</strong> juon mehua.
+        <br>
+        <br>このシャツは<strong>白くて</strong>、あのシャツは黒いです。
+        <br><i>Kono shatsu wa shirokute, ano shatsu wa kuroi desu.</i>
+        <br>Tämä paita on valkoinen ja tuo paita tuolla on musta.
+        <br><br>
+        Jos kyseessä on substantiivi tai na-adjektiivi, yhdistetään lauseet laittamalla sanan perään <strong>で</strong>
+        <br>マリさんは弁護士<strong>で</strong>、マリオさんは看護師です。
+        <br><i>Mari-san wa bengoshi de, Mario-san wa kangoshi desu.</i>
+        <br>Mari on asianajaja <strong>ja</strong> Mario on sairaanhoitaja.
+        <br>
+        <br>自然が<strong>好きで</strong>、よく森を散歩します。
+        <br><i>Shizen ga suki de, yoku mori o sanpo shimasu.</i>
+        <br>Tykkään luonnosta <strong>ja</strong> käyn usein metsässä kävelyillä.
+        <br>(Huom. japanissa tykätä ei ole verbi vaan na-adjektiivi!)`}
 ]
 
 export {tiedot};
