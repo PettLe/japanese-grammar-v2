b9c1a27d8eb272793947f68049d5450dd9814935
Petteri Leino <petewaterz@gmail.com>
2024-02-16 17:26:25 +0200

    created little form for search. Still not functioning

--- japanese-grammar-help/app/dictionary/page.js
+++ japanese-grammar-help/app/dictionary/page.js
@@ -1,3 +1,5 @@
+import React from "react";
+import {Input} from "@nextui-org/react";
 export default async function Dictionary() {
     const testiSana = "denki"
     // JLPT dictionary `https://jlpt-vocab-api.vercel.app/api/words?word=夜更かし`
@@ -17,10 +19,21 @@ export default async function Dictionary() {
     }
     })
 
+    async function handleClick(data) {
+        'use server';
+        console.log(data.get("inputField"))
+    }
+
     return (
       <main className="bg-neutral-950 flex min-h-screen flex-col items-center justify-between p-24">
         <div className="flex flex-col z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1 className="text-slate-50 text-lg p-5 my-5">SANAKIRJA</h1>
+
+            <div className="flex w-1/4 flex-wrap md:flex-nowrap gap-4 m-5 place-items-center">
+                <form action={handleClick}>
+            <Input name="inputField" type="search" label="Hae sanaa:" placeholder="Haku" className="p-50 text-slate-900"/><button type="submit" className="p-2 bg-neutral-500 rounded-md">Hae</button>
+            </form>
+    </div>
         </div>
       </main>
     );
