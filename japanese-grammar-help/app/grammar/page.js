fd79bdd64d492045538398da9a871a8d3209fa76
Petteri Leino <petewaterz@gmail.com>
2024-02-26 13:37:56 +0200

    real-time search for desired grammar points created

--- japanese-grammar-help/app/grammar/page.js
+++ japanese-grammar-help/app/grammar/page.js
@@ -3,19 +3,33 @@
 import React from "react";
 import {Card, CardHeader, CardBody, Divider} from "@nextui-org/react";
 import {data} from "./grammarData.js"
+import { useState } from 'react'
 
-
-export default async function Grammar() {
+export default function Grammar() { //aikaisemmin oli async-funktio, älä unohda
+    const [query, setQuery] = useState('');
     const toggleCard = function(e){
         const toggle = e.target.querySelector(".toggleThis")
         toggle.classList.toggle("hidden")
-
     }
-// Alkuperäinen kortti bg-zinc-500
+
+    const filter = (array) => {
+      return array.filter(el => el.title.toLowerCase().includes(query))}
+
+    const filtered = filter(data)
+
+    const handleChange = (e) => {
+      setQuery(e.target.value)
+      console.log("toimii")
+      }
+
+  
     return (
       <main className="bg-neutral-950 flex min-h-screen flex-col items-center justify-between p-24">
         <div className="flex flex-col z-10 w-5xl w-full items-center justify-between font-mono text-sm">
-        {data.map((card) => (
+          <div className="w-3/4 text-teal-500 text-lg font-bold text-left">
+        <input className="p-50 text-slate-900 self-start" onChange={handleChange} type="text" placeholder="Etsi.."/>
+        </div>
+        {filtered.map((card) => (
             card.title == "chapterLabel"     
         ? (<h1 className="w-3/4 text-teal-500 text-lg font-bold text-left">{card.content}</h1>)
         : (<Card
