a0ca44b9abda4b4326a385ea0464ef0206b6ffd3
Petteri Leino <petewaterz@gmail.com>
2024-02-28 10:17:26 +0200

    random word generator created. Button for it too, but not yet functional

--- japanese-grammar-help/app/page.js
+++ japanese-grammar-help/app/page.js
@@ -1,12 +1,37 @@
+"use client"
+
 import {Image} from "@nextui-org/react";
 
-export default function Home() {
+export default async function Home() {
+    
+    // Fetch API-kutsu, jolla haetaan satunnainen japaninkielinen sana
+    let randomWord = await fetch(`https://jlpt-vocab-api.vercel.app/api/words/random`, { cache: 'no-store' }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data)
+        return data;
+    })
+
+    const generateNewWord = function(e){
+      console.log("CLICK")
+      const word = e.target.querySelector(".word")
+      const meaning = e.target.querySelector(".meaning")
+      console.log(word + " " + meaning)
+      // toggle.classList.toggle("hidden")
+  }
+
   return (
     <main className="bg-neutral-950 text-teal-100 flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="flex-column">
       <h1 className="text-lg animate-bounce">Terve</h1>
       <p>Teretulemast oppimaan japanin kielioppia ja sellasta, ehkä joku parempi teksti tähän hei myöhemmin.</p>
+      <div className="my-5"><h3>Ennen kuin jatkat, ota matkaasi uusi sana!</h3>
+      <br />
+    <p className="word">Sana: {randomWord.word} ({randomWord.furigana})</p>
+    <p className="meaning">Merkitys: {randomWord.meaning}</p>
+    <button onClick={(e) => generateNewWord(e)} className="bg-transparent hover:bg-teal-100 text-teal-100 font-semibold hover:text-black py-2 px-4 border border-teal-100 hover:border-transparent rounded">Arvo uusi sana</button>
+    </div>
       </div>
       <Image className="rounded-xl"
       width={600}
